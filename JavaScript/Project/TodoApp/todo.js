f24955c77700fefbeb32377777cde117514bd251
shakil Ahamad <Shakil91021@gmail.com>
2024-03-23 03:40:14 +0530

    first commit

---
+++ JavaScript/Project/TodoApp/todo.js
@@ -0,0 +1,42 @@
+let todoList=[{
+  item:'apple',
+  dueDate:'4/12/2023'
+},
+{
+  item:'mango',
+  dueDate:'1/2/2024'
+}
+];
+displayItems();
+function addTodo(){
+  let inputElement=document.querySelector('#todo-input');
+  let dateElement=document.querySelector('#todo-date');
+
+  let todoItem=inputElement.value;
+  let todoDate=dateElement.value;
+  todoList.push({item:todoItem,
+                  dueDate:todoDate
+  });
+  inputElement.value='';
+  todoDate.value='';
+  displayItems();
+}
+
+
+function displayItems(){
+let containerElement=document.querySelector('.todo-container');
+let newHtml='';
+
+for(let i=0;i<todoList.length;i++){
+  // let item=todoList[i].item;
+  // let dueDate=todoList[i].dueDate;
+  let{item,dueDate}=todoList[i];
+  newHtml+=`
+  <span>${item}</span><span>${dueDate}</span>
+  <button class="btn-delete"onclick="todoList.splice(${[i]},1);
+   displayItems();">Delete</button>
+
+ `;
+containerElement.innerHTML=newHtml;
+}
+}
